c3b6627bc95585ce7d7e2da5d8a6ec8ec5d70dda
vankha1 <vovankha2003@gmail.com>
2023-06-10 23:21:41 +0700

    Deal with problem "Too many index.js"

---
+++ src/components/Image/Image.js
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import styles from './Image.module.scss';
+import { forwardRef, useState } from 'react';
+import images from '~/assets/images';
+
+//fallback: customFallback = images.noImage --> when Image element has prop fallback and is undefined, it will be set to customFallback. But if customFallback is still undefined, it will be set to images.noImage.
+
+function Image({ src, alt, className, fallback: customFallback = images.noImage, ...props }, ref) {
+    const [fallback, setFallback] = useState('');
+
+    const handleError = () => {
+        setFallback(customFallback);
+    };
+
+    return (
+        <img
+            className={classNames(styles.wrapper, className)} // className is used for future when you want to push more class in this image
+            src={fallback || src}
+            alt={alt}
+            ref={ref}
+            {...props}
+            onError={handleError}
+        />
+    );
+}
+
+Image.propTypes = {
+    src: PropTypes.string,
+    alt: PropTypes.string,
+    className: PropTypes.string,
+    fallback: PropTypes.string,
+} 
+
+export default forwardRef(Image);
--- src/components/Popper/Menu/MenuItem.js
+++ src/components/Popper/Menu/MenuItem.js
@@ -1,21 +1,25 @@
 import PropTypes from 'prop-types';
-import Button from "~/components/Button";
+import Button from '~/components/Button/Button';
 import classNames from 'classnames/bind';
 import styles from './Menu.module.scss';
 
 const cx = classNames.bind(styles);
 
-function MenuItem({ data, onClick}) {
+function MenuItem({ data, onClick }) {
     const classes = cx('menu-item', {
         separate: data.separate,
     });
 
-    return <Button className={classes} leftIcon={data.icon} to={data.to} onClick={onClick}>{data.title}</Button>
+    return (
+        <Button className={classes} leftIcon={data.icon} to={data.to} onClick={onClick}>
+            {data.title}
+        </Button>
+    );
 }
 
 MenuItem.propTypes = {
-    data : PropTypes.object.isRequired,
-    onClick : PropTypes.func
-}
+    data: PropTypes.object.isRequired,
+    onClick: PropTypes.func,
+};
 
 export default MenuItem;
